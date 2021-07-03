c0dbf244629273e80dcfac32adc088b8e0005580
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-07-03 18:35:39 +0700

    preferencePage

---
+++ Maluscation/Controller/SwipePageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SwipePageViewController.swift
+//  Maluscation
+//
+//  Created by Gilbert Nicholas on 03/07/21.
+//
+
+import UIKit
+
+class SwipePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

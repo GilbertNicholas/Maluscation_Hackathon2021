e2b5a8871fd3d56a267ff707ecc9abf2232172a4
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-07-03 11:45:36 +0700

    BASE PROJEK

--- Maluscation/ViewController.swift
+++ Maluscation/ViewController.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var review: Review!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        review.reviewToUser?.name
+        
         // Do any additional setup after loading the view.
     }
 

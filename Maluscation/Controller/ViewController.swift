c49376ba2f1280f1cfa46b65ba8a3a75cec624fc
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-07-03 11:52:05 +0700

    PULL!

--- Maluscation/Controller/ViewController.swift
+++ Maluscation/Controller/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        review.reviewToUser?.name
         
         // Do any additional setup after loading the view.
     }

d1001446c057866bc074b40850d817d0ad0a8f14
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-07-04 10:14:41 +0700

    FIX COre DATA MODEL

--- Maluscation/Controller/SwipePageViewController.swift
+++ Maluscation/Controller/SwipePageViewController.swift
@@ -8,22 +8,69 @@
 import UIKit
 
 class SwipePageViewController: UIViewController {
+    
+    @IBOutlet private weak var buttonContainer: UIView!
+    @IBOutlet private weak var cardContainer: UIView!
+    
+    @IBOutlet private weak var activeBtn: UIButton!
+    @IBOutlet private weak var casualBtn: UIButton!
+    @IBOutlet private weak var natureBtn: UIButton!
+    @IBOutlet private weak var chillBtn: UIButton!
+    @IBOutlet private weak var prevBtn: UIButton!
+    
+    @IBOutlet private weak var cardImage: UIImageView!
+    var chosenPref: String!
+    
+    private var divisor: CGFloat!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        divisor = (view.frame.width / 2) / 0.3
 
-        // Do any additional setup after loading the view.
+        setupPrefButton()
+        setupSwipeCard()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupPrefButton() {
+        buttonContainer.layer.cornerRadius = 30
+        activeBtn.layer.cornerRadius = 17
+        casualBtn.layer.cornerRadius = 17
+        natureBtn.layer.cornerRadius = 17
+        chillBtn.layer.cornerRadius = 17
+        
+        resetBorderButton()
     }
-    */
-
+    
+    private func resetBorderButton() {
+        casualBtn.layer.borderWidth = 0
+        activeBtn.layer.borderWidth = 0
+        natureBtn.layer.borderWidth = 0
+        chillBtn.layer.borderWidth = 0
+    }
+    
+    private func setupSwipeCard() {
+        cardContainer.layer.shadowColor = UIColor.black.cgColor
+        cardContainer.layer.shadowOpacity = 1
+        cardContainer.layer.shadowOffset = .zero
+        cardContainer.layer.shadowRadius = 5
+        cardContainer.layer.cornerRadius = 22
+        cardImage.layer.cornerRadius = 22
+    }
+    
+    @IBAction private func prefButtonTapped(_ sender: UIButton) {
+        resetBorderButton()
+        sender.layer.borderWidth = 3
+        self.chosenPref = sender.currentTitle
+    }
+    
+    @IBAction private func prevButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func panCardSwiped(_ sender: UIPanGestureRecognizer) {
+        let card = sender.view
+        
+    }
+    
 }

e2821b72cbd16d64e70d8ee5fc989e9088c6f4ac
FFFFSU <christiannico04@gmail.com>
2021-07-04 14:45:35 +0700

    Profile controller and vc, almost done

--- Maluscation/Controller/ProfileViewController.swift
+++ Maluscation/Controller/ProfileViewController.swift
@@ -10,6 +10,17 @@ import AuthenticationServices
 
 class ProfileViewController: UIViewController {
     
+    
+    @IBOutlet weak var nameContainer: UIView!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: PaddingLabel!
+    
+    @IBOutlet weak var emailContainer: UIView!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    @IBOutlet weak var phoneContainer: UIView!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    
     @IBOutlet weak var signOutButton: UIButton!
     
     private let signInButton = ASAuthorizationAppleIDButton()
@@ -20,6 +31,12 @@ class ProfileViewController: UIViewController {
         view.addSubview(signInButton)
         signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
         signOutButton.addTarget(self, action: #selector(didTapSignOut), for: .touchUpInside)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        setupViews()
     }
     
     override func viewDidLayoutSubviews() {
@@ -47,19 +64,61 @@ class ProfileViewController: UIViewController {
     func signInCheck() {
         guard let isSignedIn = UserDefaults.standard.value(forKey: "isSignedIn") as? Bool else { return }
         if isSignedIn {
+            
+            guard let firstName = UserDefaults.standard.value(forKey: "firstName") as? String,
+                  let lastName = UserDefaults.standard.value(forKey: "lastName") as? String,
+                  let email = UserDefaults.standard.value(forKey: "email") as? String
+            else { return }
+            
             signOutButton.isHidden = false
             signInButton.isHidden = true
+            emailContainer.isHidden = false
+            phoneContainer.isHidden = false
+            
+            nameLabel.text = "\(firstName) \(lastName)"
+            emailLabel.text = "\(email)"
         } else {
             signOutButton.isHidden = true
             signInButton.isHidden = false
+            emailContainer.isHidden = true
+            phoneContainer.isHidden = true
+            nameLabel.text = "Sign In"
         }
-        guard let firstName = UserDefaults.standard.value(forKey: "firstName") as? String,
-              let lastName = UserDefaults.standard.value(forKey: "lastName") as? String
-        else { return }
+    }
+    
+    private func setupViews() {
+        profileImage.setCornerRadius(ratio: cornerRadiusRatio * 3)
         
+        nameContainer.setBorderWidth(point: 3)
+        nameContainer.setBorderColor(color: UIColor.black)
+        nameContainer.setCornerRadius(ratio: cornerRadiusRatio * 3)
+        nameLabel.setCornerRadius(ratio: cornerRadiusRatio * 3)
+        
+        emailContainer.setBorderWidth(point: 3)
+        emailContainer.setBorderColor(color: UIColor.black)
+        emailContainer.setCornerRadius(ratio: cornerRadiusRatio * 3)
+        
+        phoneContainer.setBorderWidth(point: 3)
+        phoneContainer.setBorderColor(color: UIColor.black)
+        phoneContainer.setCornerRadius(ratio: cornerRadiusRatio * 3)
+        
+        signOutButton.setCornerRadius(ratio: cornerRadiusRatio * 3)
     }
 }
     
+extension UIView {
+    func setBorderWidth(point: CGFloat) {
+        layer.borderWidth = point
+    }
+    
+    func setBorderColor(color: UIColor) {
+        layer.borderColor = color.cgColor
+    }
+    
+    func setCornerRadius(ratio: CGFloat) {
+        layer.cornerRadius = ratio * min(frame.width, frame.height)
+    }
+}
 
 extension ProfileViewController: ASAuthorizationControllerDelegate {
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
@@ -70,12 +129,21 @@ extension ProfileViewController: ASAuthorizationControllerDelegate {
         switch authorization.credential {
         case let credentials as ASAuthorizationAppleIDCredential:
             UserDefaults.standard.setValue(true, forKey: "isSignedIn")
-            signInCheck()
+            
+            
             guard let firstName = credentials.fullName?.givenName,
                   let lastName = credentials.fullName?.familyName,
                   let email = credentials.email
-            else { return }
-            UserDefaults.standard.register(defaults: ["firstName": "\(firstName)", "lastName": "\(lastName)", "email": email])
+            else {
+                signInCheck()
+                return
+            }
+            
+            UserDefaults.standard.setValue(firstName, forKey: "firstName")
+            UserDefaults.standard.setValue(lastName, forKey: "lastName")
+            UserDefaults.standard.setValue(email, forKey: "email")
+
+            signInCheck()
             break
         default:
             break
